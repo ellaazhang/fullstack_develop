7aa1b0eb93becf7367c5c06523d0d95cf5e2d5e3
Zhang <elzhang@ad4.ad.alcatel.com>
2018-01-01 11:06:06 +0800

    day3 source code

---
+++ day3/decoding.py
@@ -0,0 +1,6 @@
+#!-*- coding utf-8 -*-
+msg = u"我爱北京天安门"
+
+msg
+
+print(msg)
---
+++ day3/fuzhi.py
@@ -0,0 +1,19 @@
+# name = "Alex Li"
+#
+# name2 = name
+#
+# print(name, name2)
+#
+# name = "Jack"
+#
+# print("What is the value of name2 now?")
+# print(name,name2)
+
+age=21
+
+print(age)
+
+del age
+
+age=24
+
